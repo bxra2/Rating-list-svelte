7c384565e2a345987d8929900e99b59fd347595a
baraaashraf <baraaashraf@protonmail.com>
2024-06-13 16:38:37 +0800

    rating list

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import FeedbackList from "./lib/components/FeedbackList.svelte";
+  import FeedbackStats from "./lib/components/FeedbackStats.svelte";
+  import FeedbackForm from "./lib/components/FeedbackForm.svelte";
+
+
+</script>
+
+<main class="container">
+  <FeedbackForm />
+  <FeedbackStats/>
+  <FeedbackList />
+</main>
+
+<style>
+</style>
---
+++ src/lib/components/FeedbackList.svelte
@@ -0,0 +1,11 @@
+<script>
+  import FeedbackItem from "./FeedbackItem.svelte";
+  import { FeedbackStore } from "../../stores";
+  import { fade, scale } from "svelte/transition";
+</script>
+
+{#each $FeedbackStore as f (f.id)}
+  <div in:scale out:scale={{ duration: 300 }}>
+    <FeedbackItem item={f} />
+  </div>
+{/each}
